0308859d12cab452f6b42a977a8f96f42fafde86
g4dalcom <g4dalcom@gmail.com>
2022-09-18 12:24:34 +0900

    complete

--- src/main/java/com/jpabook/jpashop/service/OrderService.java
+++ src/main/java/com/jpabook/jpashop/service/OrderService.java
@@ -8,9 +8,12 @@ import com.jpabook.jpashop.domain.item.Item;
 import com.jpabook.jpashop.repository.ItemRepository;
 import com.jpabook.jpashop.repository.MemberRepository;
 import com.jpabook.jpashop.repository.OrderRepository;
+import com.jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -52,8 +55,8 @@ public class OrderService {
         order.cancel();;
     }
 
-//    /* 주문 검색 */
-//    public List<Order> findOrders(OrderSearch orderSearch) {
-//        return orderRepository.findAll(orderSearch);
-//    }
+    /* 주문 검색 */
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
+    }
 }
